19fd0c1779de42482c126f8deb48c992833a8734
Andrew T <terkandrew@gmail.com>
2022-08-18 09:59:33 -0500

    Initial Commit

---
+++ WIP/WIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WIPApp.swift
+//  WIP
+//
+//  Created by admin on 8/18/22.
+//
+
+import SwiftUI
+
+@main
+struct WIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
